0e7210d73c939c0f9c4c517e17fcf9225621b7fe
krish_borole <borolekrish1@gmail.com>
2023-07-13 02:04:59 +0530

    over all porject is complete

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -8,6 +8,16 @@ const Billing = () => (
     </div>
     <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
     <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden" />billing & invoicing.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. fusce ipsum orci rhoncus aliporttitor integer pletea placerat.</p>
+
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img src={apple} alt="apple_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+        <img src={google} alt="google_playstore" className="w-[128px] h-[42px] object-contain cursor-pointer" />
+      </div>
+    </div>
   </section>
 )
 
